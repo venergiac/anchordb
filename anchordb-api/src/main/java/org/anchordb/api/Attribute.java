976bc56574251a5344a943deee5c8bf1d6580828
venergaic <giacomo.veneri@gmail.com>
2018-11-27 15:03:56 +0100

    on memory implementation

--- anchordb-api/src/main/java/org/anchordb/api/Attribute.java
+++ anchordb-api/src/main/java/org/anchordb/api/Attribute.java
@@ -1,11 +1,18 @@
 package org.anchordb.api;
 
+import java.util.Set;
 
+/**
+ * Attribute API. Can be historycal
+ * @author giacomoveneri
+ *
+ * @param <T>
+ */
 public interface Attribute<T> extends Entity {
 	
 	public AttributeType getType();
 	public T getValue();
-	
+	public Set<Attribute<T>> getHistory();
 	
 }
 
--- anchordb-api/src/main/java/org/anchordb/api/Relation.java
+++ anchordb-api/src/main/java/org/anchordb/api/Relation.java
@@ -1,7 +1,9 @@
 package org.anchordb.api;
 
+import java.util.Set;
 
 public interface Relation extends Entity{
 	public Asset getAsset1();
 	public Asset getAsset2();
+	public Set<Relation> getHistory();
 }
